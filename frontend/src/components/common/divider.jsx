004848fa41cf35fff8e7fb0e4caebf79ba54a04f
Piyush Kapoor <2k22cscys2213057@gmail.com>
2025-08-30 20:55:48 +0530

    removed

--- frontend/src/components/common/divider.jsx
+++
@@ -1,9 +0,0 @@
-export default function Divider({label}){
-    return (
-        <div className="flex items-center gap-2 my-6">
-            <div className="flex-grow h-px bg-gray-300" ></div>
-            {label && <span className="text-gray-500 text-sm">{label}</span>}
-            <div className="flex-grow h-px bg-gray-300" ></div>
-        </div>
-    );
-}
