c02c576087f55438bd713da2d204e50361c4742a
Piyush Kapoor <2k22cscys2213057@gmail.com>
2025-08-30 14:46:08 +0530

    Revert "basic code"
    This reverts commit a8928fa274d842b8fd0df21f556c933a4a59ee22.

--- backend/routes/uploadRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleUpload } = require('../controllers/uploadController');
-
-router.post('/', handleUpload);
-
-module.exports = router;
