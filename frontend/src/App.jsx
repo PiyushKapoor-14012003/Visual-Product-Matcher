e3d745c3fe86faba7ce20117f119cb15cb85d38e
Piyush Kapoor <2k22cscys2213057@gmail.com>
2025-08-30 16:30:19 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,14 @@
-// src/App.jsx
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Results from "./pages/Results";
 
-function App() {
+export default function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
     </Router>
   );
 }
-
-export default App;
